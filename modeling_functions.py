4041f24fd6a4a76b9f230e1cb27d1ca831858eb8
brendanstec <31809711+brendanstec@users.noreply.github.com>
2018-02-27 22:54:18 -0500

    Update modeling_functions.py

--- modeling_functions.py
+++ modeling_functions.py
@@ -2,19 +2,27 @@
 
 
 
-def cross_validate(X,y,k):
+def my_cross_validate(X,y,k,mod_type,scoring_type):
     '''
-    B. Stec - version 1.1 - February 2018
+    B. Stec - version 1.2 - February 2018
     Function that runs k-fold cross validation on X and y. X can either be a num rows-by-num features numpy array or a 
     num rows-by-num features pandas DataFrame. y can be either a num row-by-1 numpy array or a num row-by-1 DataFrame.
-    k is a list of cross-validation sizes (e.g. k can be [10], [2,5,10], etc.).
+    k is a list of cross-validation set sizes (e.g. k can be [10], [2,5,10], etc.). Improved to include many different
+    estimators and different scoring schemes beyond MSE. 
+    
+    regression metrics: explained_variance_score, mean_absolute_error, mean_squared_error, mean_squared_log_error,
+    median_absolute_error, r2_score
+    
+    classification metrics (ones supported): accuracy_score, classification_report, confusion_matrix, f1_score, 
+    hamming_loss, jaccard_similarity_score, log_loss, matthews_corrcoef, precision_score, recall_score, zero_one_loss
     '''
-    #return a list of average mse by different k values
-    average_mses = list()
+ 
+    #return a list of average metrics by different k values
+    average_errors = list()
     for n in k:
         kf = KFold(n_splits=n,random_state=101)
         #create a blank list for mse's per fold
-        mean_squared_errors = list()
+        errors = list()
         for train_index, test_index in kf.split(X):
             #if the data is a numpy array use normal indexing
             if type(X).__name__ == 'ndarray':
@@ -26,19 +34,19 @@ def cross_validate(X,y,k):
                 X_train, X_test = X.iloc[train_index], X.iloc[test_index]
                 y_train, y_test = y.iloc[train_index], y.iloc[test_index]
             #fit the model
-            lm.fit(X_train,y_train)
+            mod_type.fit(X_train,y_train)
             #make predictions
-            pred = lm.predict(X_test)
+            pred = mod_type.predict(X_test)
             #calculate mean squared error
-            mse=mean_squared_error(y_test,pred)
+            error=scoring_type(y_test,pred)
             #add each fold mse to list
-            mean_squared_errors.append(mse)
+            errors.append(error)
         #find the average mse per fold
-        average_mse = np.mean(mean_squared_errors)
+        average_error = np.mean(errors)
         #add average mse per fold to list of other averages
-        average_mses.append(average_mse)
+        average_errors.append(average_error)
     #return list
-    return average_mses
+    return average_errors
     
 ####
 
